e96023888d46983d6e23f096cedb25b1a54fdf0d
Luna Seifert <thepuddingdeity@gmail.com>
2022-10-11 15:40:46 -0600

    commit tree d6d6831eaa20c0739bd21fc771e4f4223930c0b5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, email){
+    let user = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": email
+        }),
+    }
+
+   return fetch('http://localhost:3000/users', user).then(res => res.json()).then(returnedUser =>{
+    const body = document.querySelector('body')
+    body.innerHTML = returnedUser.id
+
+   }).catch(message =>{
+   document.querySelector('body').innerHTML = message
+   })
+}
